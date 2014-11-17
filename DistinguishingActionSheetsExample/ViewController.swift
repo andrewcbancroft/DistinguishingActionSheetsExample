bbd9687ddccfd30c27ef6234f0d533145b146003
Andrew Bancroft <andrewcbancroft@gmail.com>
2014-11-16 22:13:56 -0600

    initial commit

--- DistinguishingActionSheetsExample/ViewController.swift
+++ DistinguishingActionSheetsExample/ViewController.swift
@@ -1,25 +1,49 @@
-//
-//  ViewController.swift
-//  DistinguishingActionSheetsExample
-//
-//  Created by Andrew Bancroft on 11/14/14.
-//  Copyright (c) 2014 Andrew Bancroft. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
+enum ActionSheetTag: Int {
+	case ActionSheet1
+	case ActionSheet2
 }
 
+class ViewController: UIViewController, UIActionSheetDelegate {
+	@IBOutlet weak var choiceLabel: UILabel!
+	
+	@IBAction func presentActionSheet1(sender: AnyObject) {
+		let actionSheet1 = UIActionSheet()
+		actionSheet1.delegate = self
+		actionSheet1.title = "Choices"
+		actionSheet1.tag = ActionSheetTag.ActionSheet1.toRaw()
+		actionSheet1.addButtonWithTitle("Choice 1")
+		actionSheet1.addButtonWithTitle("Choice 2")
+		actionSheet1.addButtonWithTitle("Cancel")
+		actionSheet1.cancelButtonIndex = 2
+		
+		actionSheet1.showInView(self.view)
+	}
+	
+	@IBAction func presentActionSheet2(sender: AnyObject) {
+		let actionSheet2 = UIActionSheet()
+		actionSheet2.delegate = self
+		actionSheet2.title = "Options"
+		actionSheet2.tag = ActionSheetTag.ActionSheet2.toRaw()
+		actionSheet2.addButtonWithTitle("Option 1")
+		actionSheet2.addButtonWithTitle("Option 2")
+		actionSheet2.addButtonWithTitle("Cancel")
+		actionSheet2.cancelButtonIndex = 2
+		
+		actionSheet2.showInView(self.view)
+	}
+	
+	func actionSheet(actionSheet: UIActionSheet, clickedButtonAtIndex buttonIndex: Int) {
+		if let tag = ActionSheetTag.fromRaw(actionSheet.tag) {
+			switch tag {
+			case .ActionSheet1:
+				self.choiceLabel.text = "Button \(buttonIndex) of Action Sheet 1 was selected."
+			case .ActionSheet2:
+				self.choiceLabel.text = "Button \(buttonIndex) of Action Sheet 2 was selected."
+			default:
+				println("Unknown action sheet.")
+			}
+		}
+	}
+}
